7e47b97f9112486232cfe6ab354594534a818769
Tim Hettler <me@timhettler.com>
2015-03-10 16:41:45 -0400

    multiple layers

--- masking/Gruntfile.js
+++ masking/Gruntfile.js
@@ -483,7 +483,7 @@ var taskConfig = {
     connect : {
         options: {
           port: 9000,
-          livereload: 35729,
+          //livereload: 35729,
           // change this to '0.0.0.0' to access the server from outside
           hostname: 'localhost'
         },
